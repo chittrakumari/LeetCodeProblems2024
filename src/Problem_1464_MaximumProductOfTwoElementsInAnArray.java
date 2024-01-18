ba3f1c8658da49076d530c642b408250648b5302
chittrakumari <chittrakumari18@gmail.com>
2024-01-18 14:04:28 +0530

    Problem_1464_MaximumProductOfTwoElementsInAnArray

--- src/Problem_1464_MaximumProductOfTwoElementsInAnArray.java
+++ src/Problem_1464_MaximumProductOfTwoElementsInAnArray.java
@@ -1,3 +1,13 @@
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+*
+* */
+
 public class Problem_1464_MaximumProductOfTwoElementsInAnArray {
     public int maxProduct(int[] nums) {
             int max=Integer.MIN_VALUE;
